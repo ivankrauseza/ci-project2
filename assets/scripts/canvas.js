28d01f404365a6cd6727e99971a1392e2d17c9a4
Ivan Krause <ivan.krause@gmail.com>
2023-06-05 21:24:08 +0100

    add canvas config in canvas.js

--- assets/scripts/canvas.js
+++ assets/scripts/canvas.js
@@ -1,3 +1,13 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+canvas.width = 1024;
+canvas.height = 1024;
+
+
+function draw() {
+    console.log('draw');
+}
+
 
 function caption() {
     console.log('caption');
