689f07b2205e64b4ad9f65cdcca15246964f2be7
Ivan Krause <ivan.krause@gmail.com>
2023-06-05 22:12:27 +0100

    Draw text caption to canvas when choosing file

--- assets/scripts/canvas.js
+++ assets/scripts/canvas.js
@@ -4,7 +4,7 @@ canvas.width = 1024;
 canvas.height = 1024;
 
 // READ IMAGE DATA
-let uploadFile = new Image();
+let uploadFile = new Image(); // This is the image for the canvas.
 let imgWdt;
 let imgHgt;
 let imgInput = document.getElementById('file');
@@ -18,22 +18,56 @@ imgInput.addEventListener('change', function(e) {
 
         reader.readAsDataURL(imageData);
 
-        reader.onloadend = function () {
-            console.log(imageData);
+        reader.onloadend = function(e) {
+        
+            uploadFile.src = e.target.result; // Assign image source
+
+            uploadFile.onload = function() {
+                
+                // ASSIGN & SHOW SIZE
+                imgWdt = uploadFile.width;
+                imgHgt = uploadFile.height;
+
+                console.log(imgWdt+"x"+imgHgt);
+
+                // GET ORIENTATION
+                if(imgWdt < imgHgt) {
+                    console.log('portrait');
+                }
+                else if(imgWdt > imgHgt) {
+                    console.log('landscape');
+                }
+                else if(imgWdt == imgHgt) {
+                    console.log('square');
+                }
+                else {
+                    console.log('unknown');
+                }
+
+                draw();
+            }
         }
     } 
-
 });
 
 // ADD CAPTION TO THE CANVAS
-function caption(event) {
-    event.preventDefault();
-    console.log('caption');
+function caption() {
+    //console.log('caption');
+    ctx.font = "65px Impact";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "top";
+    ctx.fillStyle = "white";
+    ctx.fillText(
+        "This is the caption".toUpperCase(), // Text content
+        canvas.width / 2, // Start left
+        0, // Start Top
+    )
 }
 
 // DRAW THE FINAL CANVAS
 function draw() {
     console.log('draw');
+    caption();
 }
 
 // DOWNLOAD THE FINAL DRAWING
