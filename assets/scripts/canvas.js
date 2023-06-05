804ec878fcc2a0e1c5cd4896b477005d21cf9697
Ivan Krause <ivan.krause@gmail.com>
2023-06-05 21:49:19 +0100

    read the chosen file data

--- assets/scripts/canvas.js
+++ assets/scripts/canvas.js
@@ -3,20 +3,50 @@ let ctx = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 1024;
 
+// READ IMAGE DATA
+let uploadFile = new Image();
+let imgWdt;
+let imgHgt;
+let imgInput = document.getElementById('file');
 
+imgInput.addEventListener('change', function(e) {
+    
+    if(e.target.files) {
+        
+        let imageData = e.target.files[0]; // Here we assign the file data
+        let reader = new FileReader(); // Read the file function
+
+        reader.readAsDataURL(imageData);
+
+        reader.onloadend = function () {
+            console.log(imageData);
+        }
+    } 
+
+});
+
+// ADD CAPTION TO THE CANVAS
+function caption(event) {
+    event.preventDefault();
+    console.log('caption');
+}
+
+// DRAW THE FINAL CANVAS
 function draw() {
     console.log('draw');
 }
 
-
-function caption() {
-    console.log('caption');
+// DOWNLOAD THE FINAL DRAWING
+function download() {
+    console.log('download');
 }
 
+// RESET THE CANVAS
 function reset() {
     console.log('reset');
 }
 
-function download() {
-    console.log('download');
+// DEFAULT ONLOAD
+window.onload = () => {
+    localStorage.clear();
 }
